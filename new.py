91e06985b464744ab3d9698e840eec0072eb03bf
Roodraksh bisen <roodrakshbisen@gmail.com>
2025-09-30 10:49:47 +0530

    Adding ne.py

---
+++ new.py
@@ -0,0 +1,10 @@
+import mysql.connector 
+connector = mysql.connector.connect(host="localhost",user="root",password="superb$1839S",database="college")
+print("Database seelected")
+cursor = connector.cursor()
+cursor.execute("""insert into student(ST_ID, STUDENT_NAME, MOBILE_NO,DEPARTMENT)
+               values(3, "SAMEER","89898989","CSE");""")
+connector.commit()
+cursor.close()
+connector.close()
+print("Entered the data sucessfully")
